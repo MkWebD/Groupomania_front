c9af916afc21b5ccce647e12c588264a9fe122c2
Kevin Mas <maskevin.w@gmail.com>
2022-08-25 15:44:01 +0200

    final commit

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
     devServer: {
-      proxy: 'http://localhost:3000/'
+      proxy: 'https://mkwebd-groupomania-back.herokuapp.com/'
     },
   pluginOptions: {
     vuetify: {
