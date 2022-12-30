ac406ccb975b29112adcb4b81fb1fe9862c379aa
Kevin Mas <kmas@thess-corp.fr>
2022-12-30 16:44:08 +0100

    fix: updating backend

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-    devServer: {
-      proxy: 'https://mkwebd-groupomania-back.herokuapp.com/'
-    },
-  pluginOptions: {
-    vuetify: {
+	transpileDependencies: true,
+	devServer: {
+		proxy: "https://Groupomaniaback.kevinmas.repl.co/",
+	},
+	pluginOptions: {
+		vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+		},
+	},
+});
