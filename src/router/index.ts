b6d7aa6c17e2c587dbdbb4a65b1c19202b8b9573
Kevin Mas <kmas@thess-corp.fr>
2023-01-02 06:59:34 +0100

    fix: update ErrorPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,52 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: () => require("../views/posts/AllPosts.vue"),
-    },
-    {
-        path: "/:id",
-        name: "post",
-        component: () => require("../views/posts/SinglePost.vue")
-    },
-    {
-        path: "/newPost",
-        name: "postCreation",
-        component: () => require("../views/posts/CreatePost.vue")
-    },
-    {
-        path: "/:id",
-        name: "postUpdate",
-        component: () => require("../views/posts/UpdatePost.vue")
-    },
-    {
-        path: "/auth/sign_up",
-        name: "signUp",
-        component: () => require("../views/auth/SignUpView.vue"),
-    },
-    {
-        path: "/auth/log_in",
-        name: "logIn",
-        component: () => require("../views/auth/LogInView.vue"),
-    },
+	{
+		path: "/",
+		name: "home",
+		component: () => require("../views/posts/AllPosts.vue"),
+	},
+	{
+		path: "/:id",
+		name: "post",
+		component: () => require("../views/posts/SinglePost.vue"),
+	},
+	{
+		path: "/newPost",
+		name: "postCreation",
+		component: () => require("../views/posts/CreatePost.vue"),
+	},
+	{
+		path: "/:id",
+		name: "postUpdate",
+		component: () => require("../views/posts/UpdatePost.vue"),
+	},
+	{
+		path: "/auth/sign_up",
+		name: "signUp",
+		component: () => require("../views/auth/SignUpView.vue"),
+	},
+	{
+		path: "/auth/log_in",
+		name: "logIn",
+		component: () => require("../views/auth/LogInView.vue"),
+	},
+	{
+		path: "/:pathMatch(.*)*",
+		name: "error404",
+		component: "ErrorPage",
+	},
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 router.beforeEach((to) => {
-    if (!localStorage.user && to.name !== 'logIn' && to.name !== 'signUp') {
-        return {name: 'logIn'}
-    }
-})
+	if (!localStorage.user && to.name !== "logIn" && to.name !== "signUp") {
+		return { name: "logIn" };
+	}
+});
 
 export default router;
