5ce9fdc213da699f855cbd0373598b526cdff528
Kevin Mas <kmas@thess-corp.fr>
2023-08-30 07:28:39 +0200

    refactor: formatted files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-    <v-app>
-        <v-main>
-            <NavBar/>
-            <router-view/>
-        </v-main>
-    </v-app>
+	<v-app>
+		<v-main>
+			<NavBar />
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script setup lang="ts">
-    import NavBar from "./components/NavBar.vue"
-    import '../src/assets/styles/global.scss'
+import "../src/assets/styles/global.scss";
+import NavBar from "./components/NavBar.vue";
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
